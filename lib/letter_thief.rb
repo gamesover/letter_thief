3d8b4fe61e5c2caf00e862b20e56ccad8c9a144e
Alessandro Rodi <alessandro.rodi@renuo.ch>
2025-04-06 22:35:03 +0200

    Show space used in the Outbox

--- lib/letter_thief.rb
+++ lib/letter_thief.rb
@@ -2,5 +2,11 @@ require "letter_thief/version"
 require "letter_thief/engine"
 
 module LetterThief
-  # Your code goes here...
+  def self.used_activestorage_space
+    ActiveStorage::Blob
+      .joins(:attachments)
+      .where(active_storage_attachments: {
+        record_type: "LetterThief::EmailMessage"
+      }).sum(:byte_size)
+  end
 end
